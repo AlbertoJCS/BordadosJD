adf3c0700a92171dea623bdc70830f3829edc06b
AlbertoJCS <premier.jcs@gmail.com>
2023-08-20 22:43:38 -0600

    updated

--- js/contacto.js
+++ js/contacto.js
@@ -1,11 +1,61 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const fechaNacimiento = document.getElementById('fechaNacimiento');
-    const edadInput = document.getElementById('edad');
+const app = Vue.createApp({
+  data() { //Data es una función que devuelve un objeto
+    //El objeto que se retorna tiene 2 propiedades: FormData y Errors
+    return {
+      FormData: { //FormData es el objeto que retorna data()
+        nombre: '',
+        email: '',
+        fechaEntrega: null,
+        genero: '', // Almacena el valor seleccionado ('Masculino' o 'Femenino')
+        cantProductos: null,
+        provincias: '' // Almacena la provincia seleccionada
+      },
+      errors: {}
+    }; 
+  },
+  methods: {
+    submitForm() {
+      // Lógica de validación
+      this.errors = {};
 
-    fechaNacimiento.addEventListener('input', function() {
-      const fecha = new Date(this.value);
-      const hoy = new Date();
-      const edad = hoy.getFullYear() - fecha.getFullYear();
-      edadInput.value = edad;
-    });
+      if (!this.formData.nombre) {
+        this.errors.nombre = 'Por favor ingresa tu nombre completo.';
+      }
+
+      if (!this.formData.email) {
+        this.errors.email = 'Por favor ingresa tu correo electrónico.';
+      } else if (!/\S+@\S+\.\S+/.test(this.formData.email)) {
+        this.errors.email = 'Por favor ingresa un correo electrónico válido.';
+      }
+
+      if (!this.formData.fechaEntrega) {
+        this.errors.fechaEntrega = 'Por favor ingrese una fecha de Entrega.';
+      }
+
+      if (!this.formData.genero) {
+        this.errors.genero = 'Porfavor Seleccione un Genero';
+      }
+
+      if (!this.formData.cantProductos) {
+        this.errors.cantProductos = 'Porfavor Seleccione la cantidad de productos deseados';
+      }
+
+      if (!this.formData.provincias) {
+        this.errors.provincias = 'Porfavor Seleccione una provincia';
+      }
+
+      // Agrega más lógica de validación aquí según tus requisitos
+
+      // Si no hay errores, puedes enviar los datos
+      if (Object.keys(this.errors).length === 0) {
+        // Aquí enviarías los datos del formulario
+        console.log('Formulario válido, enviando datos...');
+      }
+    }
+    //clearForm(){
+
+    //}
+  }
 });
+
+app.mount('#app');
